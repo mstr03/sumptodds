82c10afa90679dacd5e80ceeb34916e45fb24c04
Michał Strzelczyk <michciustrzelczyk@gmail.com>
2021-11-01 16:22:20 +0100

    Init project

---
+++ apps/web/config/routes.rb
@@ -0,0 +1,6 @@
+# Configure your routes here
+# See: https://guides.hanamirb.org/routing/overview
+#
+# Example:
+# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+get '/base', to: 'base#index'
